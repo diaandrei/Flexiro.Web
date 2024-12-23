174b0a2e8cd6e70cdcb5a939686eb878f2ccd0a7
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-12-23 20:49:20 +0000

    Adding an api and slice to handle the sign up process

---
+++ src/features/sign-up/signUpApi.js
@@ -0,0 +1,14 @@
+import { faL } from "@fortawesome/free-solid-svg-icons";
+import { postRequest } from "../../api/api";
+export const signUpAPI = async (userData) => {
+  try {
+    const response = await postRequest("/register", userData);
+    return response.data;
+  }
+  catch (error) {
+
+    throw new Error(
+      error.response?.data?.content?.title || error.message
+    );
+  }
+};
---
+++ src/features/sign-up/signUpSlice.js
@@ -0,0 +1,61 @@
+// signUpSlice.js
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { signUpAPI } from './signUpApi';
+import toast from 'react-hot-toast';
+
+export const signUpUser = createAsyncThunk(
+    'signup/signUpUser',
+    async (userData, { rejectWithValue }) => {
+        try {
+            const response = await signUpAPI(userData);
+
+            debugger;
+            if (response.success == false) {
+                const errorMessage = response.title || "Sign-up failed. Please check your information and try again.";
+                toast.error(errorMessage);
+                return rejectWithValue(response.title || "Sign-up failed. Please check your information and try again.");
+            }
+
+            if (response.success == true) {
+                const successMessage = response.titile || "Sign Up Success!";
+
+                toast.success(successMessage);
+                return response.content;
+            }
+
+            return response.data;
+        } 
+        catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+);
+
+const signUpSlice = createSlice({
+    name: 'signup',
+    initialState: {
+        authStatus: 'idle',
+        authError: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(signUpUser.pending, (state) => {
+                state.status = 'loading'; 
+                state.error = null; 
+            })
+            .addCase(signUpUser.fulfilled, (state) => {
+                state.status = 'succeeded'; 
+            })
+            .addCase(signUpUser.rejected, (state, action) => {
+                state.status = 'failed'; 
+                state.error = action.payload;
+            });
+    },
+});
+
+export const selectAuthStatus = (state) => state.signup.authStatus;
+export const selectAuthError = (state) => state.signup.authError;
+
+export default signUpSlice.reducer;
